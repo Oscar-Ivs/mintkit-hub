b9ad1ec3caa0731465b1dfd4564e5000cd4de904
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-11-26 14:25:26 +0000

    Update base dir

--- mintkithub/settings.py
+++ mintkithub/settings.py
@@ -61,10 +61,11 @@ ROOT_URLCONF = 'mintkithub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -72,6 +73,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'mintkithub.wsgi.application'
 
