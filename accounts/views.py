760424a85ecbf2a2a8ce9dd47abf7ee38d344b09
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-11-26 16:02:31 +0000

    Add accounts app login, logout and registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    """
+    Simple registration view using Django's built-in UserCreationForm.
+    """
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # create the new user
+            messages.success(request, "Your account has been created. You can now log in.")
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
