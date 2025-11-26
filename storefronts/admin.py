f029ee3636d3e3e1a2484d3d75e099a928d23092
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-11-26 16:56:58 +0000

    Create Storefront model migrations and admin registration

--- storefronts/admin.py
+++ storefronts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Storefront
 
-# Register your models here.
+
+@admin.register(Storefront)
+class StorefrontAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'is_active', 'slug', 'created_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('name', 'slug', 'owner__user__username')
+    prepopulated_fields = {'slug': ('name',)}
