cf5cfcf93c2cc13690b85ccf8cd5beeabf72f38b
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-12-01 16:57:35 +0000

    Wire subscriptions stub view and URL with placeholder template

--- core/views.py
+++ core/views.py
@@ -39,5 +39,5 @@ def studio(request):
     Later, when studio.mintkit.co.uk is mapped,
     you can update STUDIO_URL to that domain.
     """
-    STUDIO_URL = "https://studio.mintkit.co.uk/"  # change when ready
+    STUDIO_URL = "https://mintkit-smr.caffeine.xyz"  # change when ready
     return redirect(STUDIO_URL)
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,10 @@
+# subscriptions/views.py
 from django.shortcuts import render
 
-# Create your views here.
+
+def subscriptions_home(request):
+    """
+    Placeholder page for subscription management.
+    Later we'll show current plan, upgrade options, billing history, etc.
+    """
+    return render(request, "subscriptions/subscriptions_home.html")
