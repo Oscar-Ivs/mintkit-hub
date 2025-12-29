9661d7e17113bb71cd5d724e6175bf84d1a6eadb
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-12-29 09:45:07 +0000

    Add console logging for production error

--- mintkithub/settings.py
+++ mintkithub/settings.py
@@ -181,3 +181,22 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
