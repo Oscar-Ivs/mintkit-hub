6118b44d70cc321acb0aef2f3f3ef13d6cf4e00c
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-12-30 19:28:20 +0000

    Add email field to registration and welcome email

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,4 +15,16 @@ urlpatterns = [
 
     path("profile/edit/", views.edit_profile, name="edit_profile"),
 
+    path("email-preview/<str:kind>/", views.email_preview, name="email_preview"),
+
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            subject_template_name="registration/password_reset_subject.txt",
+            email_template_name="registration/password_reset_email.txt",
+            html_email_template_name="registration/password_reset_email.html",
+        ),
+        name="password_reset",
+    ),
+
 ]
