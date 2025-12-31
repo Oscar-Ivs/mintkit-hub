be2c5e98dcb9877804e259b60978e8e90b367fb9
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-12-31 18:26:32 +0000

    Show account email and add password change flow

--- accounts/urls.py
+++ accounts/urls.py
@@ -39,3 +39,14 @@ urlpatterns = [
         name="password_reset_complete",
     ),
 ]
+
+path(
+    "password/change/",
+    auth_views.PasswordChangeView.as_view(template_name="registration/password_change_form.html"),
+    name="password_change",
+),
+path(
+    "password/change/done/",
+    auth_views.PasswordChangeDoneView.as_view(template_name="registration/password_change_done.html"),
+    name="password_change_done",
+),
