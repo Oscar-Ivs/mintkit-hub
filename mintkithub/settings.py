c6fc876847d179041de6c78e6fe54de6e9085b1b
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2026-01-06 17:20:53 +0000

    Install python-dotenv

--- mintkithub/settings.py
+++ mintkithub/settings.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 # -------------------------
 # Core security / env
